843fddd4ae446599b22ff99c2e4f553a6a5db5cd
MD ALAMIN KAZI <kmalamin123210@gmail.com>
2024-04-28 10:43:23 +0600

    added get and post operation

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// FLnTIPjApiCLYc6x
-// CraftyFiber
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r90hnej.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,6 +24,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const database = client.db("craftyfiber");
+    const crafts = database.collection("crafts");
+
+    app.get("/craft", async (req, res) => {
+      const cursor = crafts.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/craft", async (req, res) => {
+      const newCraft = req.body;
+      console.log(newCraft);
+      const result = await crafts.insertOne(newCraft);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
